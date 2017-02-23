ebf167965b7dccf43fc2da114d125a0cea45e316
Michael Starkey <2194203s@student.gla.ac.uk>
2017-02-23 20:15:41 +0000

    removing test puts

--- lib/basic_github_search.rb
+++ lib/basic_github_search.rb
@@ -4,7 +4,6 @@ require "basic_github_search/basic_github_search_api"
 module BasicGithubSearch
   def self.gitSearch(keyword)
     result = BasicGithubSearchApi.new(keyword).search
-    puts result.inspect
     result
   end
 end
